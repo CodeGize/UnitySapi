68978cc118234fddf864dda36322a334e5fccc39
CgChen <ccggzz123@aliyun.com>
2016-04-21 23:10:42 +0800

    init
    init

---
+++ SApiClient/Assets/Sapi/Speecher.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Diagnostics;
+using System.Text;
+using UnityEngine;
+
+public class Speecher : MonoBehaviour, SocketExtra.INetComponent
+{
+    private SocketExtra m_socket;
+    private Process m_process;
+
+    protected void Awake()
+    {
+        Ins = this;
+        m_process = new Process
+        {
+            StartInfo = new ProcessStartInfo
+            {
+                FileName = "speech.exe",
+                CreateNoWindow = true,
+                WindowStyle = ProcessWindowStyle.Hidden
+            },
+        };
+        m_process.Start();
+    }
+
+    /***测试代码，可删除Start***/
+    protected IEnumerator Start()
+    {
+        yield return StartCoroutine(Connect());
+        Speak("test");
+    }
+    /***测试代码，可删除End***/
+
+    public IEnumerator Connect()
+    {
+        m_socket = new SocketExtra(this);
+        m_socket.Connect("127.0.0.1", 9903);
+        while (!m_socket.Connected)
+        {
+            yield return 1;
+        }
+    }
+
+    protected void OnDestroy()
+    {
+        if (m_process != null && !m_process.HasExited)
+            m_process.Kill();
+        m_process = null;
+    }
+
+    public static Speecher Ins;
+
+    public static void Speak(string str)
+    {
+#if UNITY_EDITOR||UNITY_STANDALONE_WIN
+        Ins.Speech(str);
+#endif
+    }
+
+    public void Speech(string str)
+    {
+        if (m_socket.Connected)
+        {
+            var bytes = Encoding.Default.GetBytes(str);
+            m_socket.SendMsg(bytes);
+        }
+    }
+
+    public bool NetReciveMsg(byte[] recivebuffer)
+    {
+        return true;
+    }
+
+    public bool NetSendMsg(byte[] sendbuffer)
+    {
+        return true;
+    }
+}
---
+++ Speech/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Linq;
+using System.Speech.Synthesis;
+using System.Text;
+using Speech.Properties;
+
+namespace Speech
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var server = new NetServer();
+            server.StartServer();
+
+            while (true)
+            {
+                var res = Console.ReadLine();
+                if (res == "exit")
+                    break;
+            }
+        }
+    }
+
+    public class NetServer : SocketExtra.INetComponent
+    {
+        private readonly Speecher m_speecher;
+
+        private readonly SocketExtra m_socket;
+
+        public NetServer()
+        {
+            m_speecher = new Speecher();
+            m_socket = new SocketExtra(this);
+        }
+
+        public void StartServer()
+        {
+            m_socket.Bind("127.0.0.1", Settings.Default.Port);
+        }
+
+        public bool NetSendMsg(byte[] sendbuffer)
+        {
+            return true;
+        }
+
+        public bool NetReciveMsg(byte[] recivebuffer)
+        {
+            var str = Encoding.Default.GetString(recivebuffer);
+            Console.WriteLine(str);
+            m_speecher.Speak(str);
+            return true;
+        }
+
+        public bool Connected { get { return m_socket.Connected; } }
+    }
+
+    public class Speecher
+    {
+        private readonly SpeechSynthesizer m_speaker;
+
+        public Speecher()
+        {
+            m_speaker = new SpeechSynthesizer();
+            var installs = m_speaker.GetInstalledVoices();
+
+            m_speaker.Volume = Settings.Default.SpeakVolume;
+            m_speaker.Rate = Settings.Default.SpeakRate;
+            var voice = Settings.Default.SpeakVoice;
+
+            var selected = false;
+            if (!string.IsNullOrEmpty(voice))
+            {
+                if (installs.Any(install => install.VoiceInfo.Name == voice))
+                {
+                    m_speaker.SelectVoice(voice);
+                    selected = true;
+                }
+            }
+            if (!selected)
+            {
+                foreach (var install in installs.Where(install => install.VoiceInfo.Culture.Name == "en-US"))
+                {
+                    m_speaker.SelectVoice(install.VoiceInfo.Name);
+                    break;
+                }
+            }
+        }
+
+        public void Speak(string msg)
+        {
+            m_speaker.Speak(msg);
+        }
+    }
+}
